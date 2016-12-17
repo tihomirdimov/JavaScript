420ee49a74c5ae1135906224ea16fae096926a1b
tihomirdimov <tihomirdimov@tihomirdimov.com>
2016-12-17 17:06:22 +0200

    exam 2016 12 16 problem 03 in progress

--- 01.JavaScript Fundamentals/02.Exams/2016-12-16/problem03.js
+++ 01.JavaScript Fundamentals/02.Exams/2016-12-16/problem03.js
@@ -1,6 +1,14 @@
 function formatHelper(input) {
+    let numbersPattern = /\d+[\s\.]+[\d]*[\s\.]*\d+/g;
     let text = input[0];
-    console.log(text);
+    result = text.replace(/\s+/g, ' ');
+    result = result.replace(/\s*\,+\s*/g, ', ');
+    result = result.replace(/\s*\!+\s*/g, '! ');
+    result = result.replace(/\s*\?+\s*/g, '? ');
+    result = result.replace(/\s*\:+\s*/g, ': ');
+    result = result.replace(/\s*\;+\s*/g, '; ');
+    result = result.replace(/\.+\s*\.+\s*\.+\s*!/g, '...!');
+    console.log(result);
 }
 
-formatHelper(['Terribly formatted text . With chaotic spacings." Terrible quoting "! Also this date is pretty confusing : 20 . 12. 16 .'])
+formatHelper(['Terribly formatted text   .  With chaotic spacings." Terrible quoting "! Also this date is pretty confusing : 20 . 12. 16 .   Make,sure to give:proper spacing where is needed... !']);
