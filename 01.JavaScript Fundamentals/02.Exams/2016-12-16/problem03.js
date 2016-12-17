00fc14952ef7e1c717d38efb8d18a38a145fce1d
tihomirdimov <tihomirdimov@tihomirdimov.com>
2016-12-17 15:36:29 +0200

    exam 2016 12 16 problem 03

--- 01.JavaScript Fundamentals/02.Exams/2016-12-16/problem03.js
+++ 01.JavaScript Fundamentals/02.Exams/2016-12-16/problem03.js
@@ -1,2 +1,6 @@
-function problem03() {
+function formatHelper(input) {
+    let text = input[0];
+    console.log(text);
 }
+
+formatHelper(['Terribly formatted text . With chaotic spacings." Terrible quoting "! Also this date is pretty confusing : 20 . 12. 16 .'])
