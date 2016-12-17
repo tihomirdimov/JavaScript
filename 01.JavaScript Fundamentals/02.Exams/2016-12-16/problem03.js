21994c313d2df5904613a9d1624b12f58c00acf8
tihomirdimov <tihomirdimov@tihomirdimov.com>
2016-12-17 22:13:58 +0200

    exam 2016 12 16 problem 03 75/100

--- 01.JavaScript Fundamentals/02.Exams/2016-12-16/problem03.js
+++ 01.JavaScript Fundamentals/02.Exams/2016-12-16/problem03.js
@@ -1,69 +1,27 @@
 function formatHelper(input) {
-    let text = input[0];
-    result = text.replace(/\s+/g, ' ');
-    let matches;
-    let quotationsBeforePattern = /\s*"\s*/g;
-    matches = quotationsBeforePattern.exec(result);
-    while (matches !== null) {
-        let replacement = matches[0].replace(" ", "");
-        result = result.replace(matches[0], replacement);
-        matches = quotationsBeforePattern.exec(result);
-    }
-
-    let quotationsAfterPattern = /\s*\\\"/g;
-    matches = quotationsAfterPattern.exec(result);
-    while (matches !== null) {
-        let replacement = matches[0].replace(" ", "");
-        result = result.replace(matches[0], replacement);
-        matches = quotationsAfterPattern.exec(result);
-    }
-
-    result = result.replace(/\s*\,+\s*/g, ", ");
-    result = result.replace(/\s*\:+\s*/g, ": ");
-    result = result.replace(/\s*\;+\s*/g, "; ");
-    result = result.replace(/\s*\?+\s*/g, "? ");
-
-    let dotWordsPattern = /\w+\s*\.+\s*\w+/g;
-    matches = dotWordsPattern.exec(result);
-    while (matches !== null) {
-        let replacement = matches[0].replace(/\s*\.+\s*/g, '. ');
-        result = result.replace(matches[0], replacement);
-        matches = dotWordsPattern.exec(result);
-    }
-
-    let dotAfterDigitsPattern = /\d\s+\./g;
-    matches = dotAfterDigitsPattern.exec(result);
-    while (matches !== null) {
-        let replacement = matches[0].replace(" ", "");
-        result = result.replace(matches[0], replacement);
-        matches = dotAfterDigitsPattern.exec(result);
-    }
-
+    let result = input[0];
+    result = result.replace(/\s*\.\s*/g, ". ");
+    result = result.replace(/\s*\,\s*/g, ", ");
+    result = result.replace(/\s*\!\s*/g, "! ");
+    result = result.replace(/\s*\?\s*/g, "? ");
+    result = result.replace(/\s*\:\s*/g, ": ");
+    result = result.replace(/\s*\;\s*/g, "; ");
+    result = result.replace(/\.\s*\.\s*\.\s*\!/g, "...!");
     let dotBeforeDigitsPattern = /\.\s+\d/g;
-    matches = dotBeforeDigitsPattern.exec(result);
+    let matches = dotBeforeDigitsPattern.exec(result);
     while (matches !== null) {
         let replacement = matches[0].replace(" ", "");
         result = result.replace(matches[0], replacement);
         matches = dotBeforeDigitsPattern.exec(result);
     }
-
-    let dotBeforeExclamationPattern = /\.\s+\!/g;
-    matches = dotBeforeExclamationPattern.exec(result);
+    let quotationPattern = /\".*\"/g;
+    matches = quotationPattern.exec(result);
     while (matches !== null) {
-        let replacement = matches[0].replace(" ", "");
+        let replacement = matches[0].replace(/\s+\"/g, `"`);
+        replacement = replacement.replace(/\"\s+/g, `"`);
         result = result.replace(matches[0], replacement);
-        matches = dotBeforeExclamationPattern.exec(result);
+        matches = quotationPattern.exec(result);
     }
-
-    let dotAndQuotationPattern = /\.\"/g;
-    matches = dotAndQuotationPattern.exec(result);
-    while (matches !== null) {
-        let replacement = matches[0].replace(`."`, `. "`);
-        result = result.replace(matches[0], replacement);
-        matches = dotAndQuotationPattern.exec(result);
-    }
-
-    result = result.replace(/\s+$/g, "");
     console.log(result);
 }
 
