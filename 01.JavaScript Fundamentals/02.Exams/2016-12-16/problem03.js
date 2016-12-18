f779cc5cdbf91d5dc44db6a83d6f54fd8fb81422
tihomirdimov <tihomirdimov@tihomirdimov.com>
2016-12-18 11:26:46 +0200

    exam 2016 12 16 problem 03 100/100

--- 01.JavaScript Fundamentals/02.Exams/2016-12-16/problem03.js
+++ 01.JavaScript Fundamentals/02.Exams/2016-12-16/problem03.js
@@ -1,28 +1,11 @@
-function formatHelper(input) {
-    let result = input[0];
-    result = result.replace(/\s*\.\s*/g, ". ");
-    result = result.replace(/\s*\,\s*/g, ", ");
-    result = result.replace(/\s*\!\s*/g, "! ");
-    result = result.replace(/\s*\?\s*/g, "? ");
-    result = result.replace(/\s*\:\s*/g, ": ");
-    result = result.replace(/\s*\;\s*/g, "; ");
-    result = result.replace(/\.\s*\.\s*\.\s*\!/g, "...!");
-    let dotBeforeDigitsPattern = /\.\s+\d/g;
-    let matches = dotBeforeDigitsPattern.exec(result);
-    while (matches !== null) {
-        let replacement = matches[0].replace(" ", "");
-        result = result.replace(matches[0], replacement);
-        matches = dotBeforeDigitsPattern.exec(result);
-    }
-    let quotationPattern = /\".*\"/g;
-    matches = quotationPattern.exec(result);
-    while (matches !== null) {
-        let replacement = matches[0].replace(/\s+\"/g, `"`);
-        replacement = replacement.replace(/\"\s+/g, `"`);
-        result = result.replace(matches[0], replacement);
-        matches = quotationPattern.exec(result);
-    }
-    console.log(result);
+function formatHelper([input]) {
+    console.log(input
+        .replace(/([.,!?:;])\s*/g,  (match, g) => g + ' ')
+        .replace(/\s+([.,!?:;])/g, (match, g) => g)
+        .replace(/\.\s*\.\s*\.\s*!/g, '...!')
+        .replace(/\.\s+(\d+)/g, (match, g) => '.' + g)
+        .replace(/"([^"]+)"/g, (match, g) => `"${g.trim()}"`));
 }
+
 
 formatHelper(['Terribly formatted text   .  With chaotic spacings." Terrible quoting "! Also this date is pretty confusing : 20 . 12. 16 .   Make,sure to give:proper spacing where is needed... ! ']);
