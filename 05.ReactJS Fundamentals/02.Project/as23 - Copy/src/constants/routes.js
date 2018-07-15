e01f0f451c69802a05f1bba01d88efd82e9e471e
Tihomir Dimov <tihomirdimov@tihomirdimov.com>
2018-07-15 15:36:10 +0300

    copy removed

--- 05.ReactJS Fundamentals/02.Project/as23 - Copy/src/constants/routes.js
+++
@@ -1,9 +0,0 @@
-export const HOME = '/';
-export const SIGN_UP = '/signup';
-export const SIGN_IN = '/signin';
-export const ADMINISTRATION = '/administration';
-export const ACCOUNT = '/account';
-export const PASSWORD_FORGET = '/forget-password';
-export const PLAYERS = '/players';
-export const GAMES = '/games';
-
